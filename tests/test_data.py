e32a4d916cbdcafdd7db9369dbcb57e0a7343301
SalazarV4 <140282366+SalazarV4@users.noreply.github.com>
2026-04-18 23:38:48 +0000

    trial

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,11 +1,12 @@
+import os
 import pandas as pd
 from src.data.load_data import load_data
 
+WORKING_DIR = os.environ["HOME"]
 def test_load_data() -> None:
-    file_path = "/workspaces/ml_practice/data/raw/credit_score.csv"
+
+    file_path = f"{WORKING_DIR}/ml_practice/data/raw/credit_score.csv"
 
     df = load_data(file_path=file_path)
 
     assert isinstance(df, pd.DataFrame)
-
-
